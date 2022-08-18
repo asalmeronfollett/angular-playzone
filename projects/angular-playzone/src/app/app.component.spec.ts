bb15cc4d98d3910d4a3f648368101c19f93ca86c
Axel Salmeron <asalmeron@follet.com>
2022-08-18 16:58:39 -0500

    test(app.component.spect.ts): expects for app-component to have its signin component

--- projects/angular-playzone/src/app/app.component.spec.ts
+++ projects/angular-playzone/src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MockComponent } from 'ng-mocks';
+import { SigninComponent } from './signin/signin.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let signinComponent: SigninComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        MockComponent(SigninComponent)
       ],
     }).compileComponents();
   });
@@ -19,4 +25,12 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should have its signinComponent child', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const counterEl = fixture.debugElement.query(By.directive(SigninComponent));
+    signinComponent = counterEl.componentInstance;
+
+    expect(signinComponent).toBeTruthy();
+  })
 });
